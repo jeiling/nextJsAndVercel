ea0534078db9dd73c1fef20a3b537ab985812a89
jeiling <a0983681642@gmail.com>
2024-10-13 17:11:54 +0800

    feat: add color to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        blue50:'#00A3FF',
+        blue100:'#00D1FF',
+        gray50:'#565656',
+        gray100:'#242424',
+        gray200:'#929292',
+        gray300:'#181818',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
